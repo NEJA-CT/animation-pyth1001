04a8b5609c26e2706713e495ca15c3833b641494
pyth1001 <jason.gillman@neja.org>
2025-12-04 16:49:10 +0000

    Have sprites and small bouncing ball

--- src/main.py
+++ src/main.py
@@ -1,30 +1,33 @@
 import pyxel
 
+GRAVITY = 0.3
+BOUNCE = .7
 W, H = 160, 120
-x, y = 10, 10
-dx, dy = 1, 1
+a = 0
+b = 1.15
 
+# initial position and velocity
+x, y = W // 2, H // 1.2
+vx, vy = 0, 0
 
+#pyxel.rect(1, 2, 3, 4, 5)
 def update():
-    global x, y, dx, dy
-    # Quit with Q
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+   global x, y, vx, vy, a, b
 
-    # Simple movement demo
-    x += dx
-    y += dy
-    if x <= 0 or x >= W - 10:
-        dx = -dx
-    if y <= 0 or y >= H - 10:
-        dy = -dy
- 
+   # Apply gravity
+   vy += GRAVITY
+   y += vy
+   a += b
+   if b >= 0:
+      b -=0.01
+   # Bounce when hitting the ground
+   ground = H - 3 #to change if ball radius changes - radius + 1
+   if y > ground:
+       y = ground
+       vy = -vy * BOUNCE # reverse and lose some energy
 
 def draw():
-    pyxel.cls(0)              # clear screen to color 0 (black)
-    pyxel.text(4, 4, "Hello Pyxel Web!", 7)
-    pyxel.rect(x, y, 10, 10, 11)  # small bouncing square
-
-
-pyxel.init(W, H, title="Pyxel Web (Codespaces)")
+   pyxel.cls(1)
+   pyxel.circ(x + a, y, 2, 9)
+pyxel.init(W, H, "Some name")
 pyxel.run(update, draw)
